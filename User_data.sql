5273e7c3580d1e0dfce6bc83f8e6d97fe7a01ad0
sgsh123 <agarwalsrishti6@gmail.com>
2017-03-17 13:46:50 +0530

    Auto Increment
    Database now has auto increment for ID

--- User_data.sql
+++ User_data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 11, 2017 at 03:03 PM
+-- Generation Time: Mar 17, 2017 at 09:14 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.24
 
@@ -39,12 +39,16 @@ CREATE TABLE `user_info` (
 --
 
 INSERT INTO `user_info` (`id`, `first name`, `last name`, `date of birth`, `contact no`) VALUES
-(0, 'X', 'Y', '2017-03-08', '1234567890'),
 (1, 'G', 'H', '0111-01-01', '3398539'),
 (2, 'D', 'F', '0002-02-01', '1313131313'),
 (3, 'V', 'S', '1999-02-01', '3293901234'),
 (4, 'S', 'A', '0111-01-01', '939329289'),
-(6, 'E', 'W', '1998-02-01', '1234567870');
+(5, 'W', 'V', '1998-01-10', '1234567890'),
+(6, 'E', 'W', '1998-02-01', '1234567870'),
+(7, 'S', 'A', '1111-11-11', '4365980823'),
+(8, 'X', 'Y', '2017-03-08', '1234567890'),
+(9, 'A', 'R', '1111-01-01', '124367575'),
+(11, 'D', 'R', '1998-01-01', '1234567890');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +60,15 @@ INSERT INTO `user_info` (`id`, `first name`, `last name`, `date of birth`, `cont
 ALTER TABLE `user_info`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
