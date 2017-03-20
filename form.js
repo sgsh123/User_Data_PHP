9491b5c1f755df5250077a4a460e4a8dc06767f6
sgsh123 <agarwalsrishti6@gmail.com>
2017-03-21 00:42:41 +0530

    Add Validation for Form Data

--- form.js
+++ form.js
@@ -1,8 +1,38 @@
-$(document).ready(function() {
-
-$("#result").on("click", function()
+$(document).ready(function()
 {
-    window.location.href='display.php';
-});    
+    $("#result").on("click", function()
+    {
+        window.location.href='display.php';
+    });    
 
 });
+
+function validate_form()
+{
+
+    var fname = document.forms["user_details"]["fname"].value;
+    var lname = document.forms["user_details"]["lname"].value;
+    var dob = document.forms["user_details"]["dob"].value;
+    var phone = document.forms["user_details"]["phone"].value;
+
+    if(fname == "" || lname == "" ||  dob == "" ||  phone == "")
+    {
+        alert("Please fill in all fields");
+        return false;
+    }
+    else
+    {
+        if(phone.length != 10)
+        {
+            alert("Please enter a valid 10 digit phone number");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+}
+
+
