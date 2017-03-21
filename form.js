5ab814c5e4f815818a11b534beafab277ee72225
sgsh123 <agarwalsrishti6@gmail.com>
2017-03-21 13:26:17 +0530

    Validation and Success Message
    Added AJAX functionality to communicate with the PHP file.

--- form.js
+++ form.js
@@ -3,36 +3,41 @@ $(document).ready(function()
     $("#result").on("click", function()
     {
         window.location.href='display.php';
-    });    
+    });
 
-});
+    $("#user_form").submit(function() {
+        event.preventDefault();
 
-function validate_form()
-{
+        var fname = document.forms["user_details"]["fname"].value;
+        var lname = document.forms["user_details"]["lname"].value;
+        var dob = document.forms["user_details"]["dob"].value;
+        var phone = document.forms["user_details"]["phone"].value;
 
-    var fname = document.forms["user_details"]["fname"].value;
-    var lname = document.forms["user_details"]["lname"].value;
-    var dob = document.forms["user_details"]["dob"].value;
-    var phone = document.forms["user_details"]["phone"].value;
-
-    if(fname == "" || lname == "" ||  dob == "" ||  phone == "")
-    {
-        alert("Please fill in all fields");
-        return false;
-    }
-    else
-    {
-        if(phone.length != 10)
+        if(fname == "" || lname == "" ||  dob == "" ||  phone == "")
         {
-            alert("Please enter a valid 10 digit phone number");
-            return false;
+            alert("Please fill in all fields");
         }
         else
         {
-            return true;
+            if(phone.length != 10)
+            {
+                alert("Please enter a valid 10 digit phone number");
+            }
+            else
+            {
+                $.ajax({  
+                    type: 'POST',  
+                    url: 'form.php', 
+                    data: { "fname": fname, "lname": lname, "dob": dob, "phone":phone},
+                    success: function(data) 
+                    {
+                        alert("Your data has been saved successfully.");
+                        window.location.reload(true);
+                    }
+               });
+            }
         }
-        
-    }
-}
+    });
+});
 
 
