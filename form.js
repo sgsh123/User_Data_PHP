d003f4a52f6ce89cc2454b4a0d2ab186f5476020
sgsh123 <agarwalsrishti6@gmail.com>
2017-03-26 01:30:02 +0530

    JQuery Normalization

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 $(document).ready(function()
 {   
-    document.getElementById("server_msg").style.visibility = "hidden";
-    document.getElementById("fname_err").style.visibility = "hidden";
-    document.getElementById("lname_err").style.visibility = "hidden";
-    document.getElementById("dob_err").style.visibility = "hidden";
-    document.getElementById("phone_err").style.visibility = "hidden";
+    $("#server_msg").css("visibility","hidden");
+    $("#fname_err").css("visibility","hidden");
+    $("#lname_err").css("visibility","hidden");
+    $("#dob_err").css("visibility","hidden");
+    $("#phone_err").css("visibility","hidden");
 
     $("#result").on("click", function()
     {
@@ -15,11 +15,11 @@ $(document).ready(function()
         event.preventDefault();
         
         //form values
-        var fname = document.forms["user_details"]["fname"].value;
-        var lname = document.forms["user_details"]["lname"].value;
-        var dob = document.forms["user_details"]["dob"].value;
-        var phone = document.forms["user_details"]["phone"].value;
-        var gender = document.querySelector('input[name="gender"]:checked').value;
+        var fname = $('#fname').val();
+        var lname = $('#lname').val();
+        var dob = $('#dob').val();
+        var phone = $('#phone').val();
+        var gender = $('input[name="gender"]:checked').val();
         
 
         //variables for validation
@@ -31,56 +31,74 @@ $(document).ready(function()
         {
             okay = false;
             $("#fname_err").text("First name should not be empty or have numbers or special characters");
-            document.getElementById("fname_err").style.color = "red";
-            document.getElementById("fname_err").style.visibility = "visible";
+            $("#fname_err").css("color","red");
+            $("#fname_err").css("visibility","visible");
+        }
+        else
+        {   
+            $("#fname_err").css("visibility","hidden");
         }
 
         if(!lname.match(alpha))
         {
             okay = false;
             $("#lname_err").text("Last name should not be empty or have numbers or special characters");
-            document.getElementById("lname_err").style.color = "red";
-            document.getElementById("lname_err").style.visibility = "visible";
+            $("#lname_err").css("color","red");
+            $("#lname_err").css("visibility","visible");
+        }
+        else
+        {
+            $("#lname_err").css("visibility","hidden");
         }
 
         if(!phone.match(num))
         {
             okay = false;
             $("#phone_err").text("Phone Number should not be empty or have numbers or special characters");
-            document.getElementById("phone_err").style.color = "red";
-            document.getElementById("phone_err").style.visibility = "visible";
+            $("#phone_err").css("color","red");
+            $("#phone_err").css("visibility","visible");
         }
         else if(phone.length != 10)
         {
             okay = false;
             $("#phone_err").text("Please enter a valid 10 digit phone number");
-            document.getElementById("phone_err").style.color = "red";
-            document.getElementById("phone_err").style.visibility = "visible";
+            $("#phone_err").css("color","red");
+            $("#phone_err").css("visibility","visible");
+        }
+        else
+        {
+            $("#phone_err").css("visibility","hidden");
         }
 
         if(dob == "")
         {
             okay = false;
             $("#dob_err").text("Date of Birth should not be empty");
-            document.getElementById("dob_err").style.color = "red";
-            document.getElementById("dob_err").style.visibility = "visible";
+            $("#dob_err").css("color","red");
+            $("#dob_err").css("visibility","visible");
+        }
+        else
+        {
+            $("#dob_err").css("visibility","hidden");
         }
         
         if(okay)
         {
+            
             $.ajax({  
             type: 'POST',  
             url: 'form.php', 
             data: { "fname": fname, "lname": lname, "dob": dob, "phone":phone, "gender": gender},
             success: function(response) 
-             {  
+             { 
                 var array = JSON.parse(response)
                 $("#server_msg").text(array.msg);
-                document.getElementById("server_msg").style.color = array.color;
-                document.getElementById("server_msg").style.visibility = "visible";    
+                $("#server_msg").css("color",array.color);
+                $("#server_msg").css("visibility","visible");
+
                 setTimeout(function() 
                 { 
-                 document.getElementById("server_msg").style.visibility = "hidden"; 
+                 $("#server_msg").css("visibility","hidden");
                  //window.location.reload(true);
                 }, 3000);
              }
